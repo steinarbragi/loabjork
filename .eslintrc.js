d9c67ac3a74cd7e79c348a5f3442a88ba295f000
Steinar Bragi Sigurðarson <s@steinar.io>
2023-12-26 20:41:03 +0000

    eslint config, autoformat, prettier, etc

---
+++ .eslintrc.js
@@ -0,0 +1,91 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.js', '.jsx'],
+      },
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+      },
+    },
+  },
+
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'prettier',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'import/no-unresolved': 'error',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      },
+    ],
+    'react/jsx-props-no-spreading': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+    'no-nested-ternary': 'off',
+    'import/prefer-default-export': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        singleQuote: true,
+        tabWidth: 2,
+        printWidth: 80,
+        arrowParens: 'avoid',
+        trailingComma: 'es5',
+        semi: true,
+        useTabs: false,
+        bracketSpacing: true,
+      },
+    ],
+  },
+};
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,8 +1,7 @@
 export default function Hero() {
-
   return (
     <div>
-        <h1 className="text-center text-3xl font-bold">Lóa Björk</h1>
+      <h1 className="text-center text-3xl font-bold">Lóa Björk</h1>
     </div>
-  )
+  );
 }
