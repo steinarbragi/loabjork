eb5dfb70ec5b12a9dda1da1bb616fe1fb679eeb1
Steinar Bragi Sigurðarson <s@steinar.io>
2023-12-28 14:24:06 +0000

    project type

---
+++ sanity/schemas/project.js
@@ -0,0 +1,47 @@
+export default {
+  name: 'project',
+  type: 'document',
+  title: 'Project',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Titill',
+    },
+    {
+      name: 'content',
+      type: 'text',
+      title: 'Content',
+    },
+    {
+      name: 'video',
+      type: 'string',
+      title: 'Video',
+    },
+    {
+      name: 'images',
+      type: 'array',
+      title: 'Images',
+      of: [
+        {
+          name: 'image',
+          type: 'image',
+          title: 'Image',
+          options: {
+            hotspot: true,
+          },
+          fields: [
+            {
+              name: 'alt',
+              type: 'string',
+              title: 'Alternative text',
+            },
+          ],
+        },
+      ],
+      options: {
+        layout: 'grid',
+      },
+    },
+  ],
+};
