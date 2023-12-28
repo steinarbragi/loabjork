f77d4cf7a0f2ad102fce314c0246a8d9b4a3fc05
Steinar Bragi Sigurðarson <s@steinar.io>
2023-12-28 23:14:21 +0000

    wip contact page

--- src/app/(loabjork)/contact/page.tsx
+++ src/app/(loabjork)/contact/page.tsx
@@ -1,3 +1,4 @@
+import ContactForm from '@/components/contactForm';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -7,10 +8,8 @@ export const metadata: Metadata = {
 
 export default function Contact() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between text-sm flex">
-        Contact
-      </div>
+    <main>
+      <ContactForm />
     </main>
   );
 }
--- src/components/social.tsx
+++ src/components/social.tsx
@@ -4,27 +4,26 @@ import LinkedInIcon from '@/icons/linkedin';
 import Link from 'next/link';
 
 export default function Social({ color }: { color?: string }) {
+  const linkClasses =
+    'bg-white/60 dark:bg-black/60 hover:drop-shadow p-4 rounded-full';
   return (
     <div
       id="social"
       className={`${
         color && color
-      } mx-auto text-center flex flex-row space-x-6 justify-center`}
+      } mx-auto text-center flex flex-row space-x-6 justify-center drop`}
     >
-      <Link
-        className="bg-white/60 dark:bg-black/60 p-4 rounded-full"
-        href="https://www.facebook.com/loabb"
-      >
+      <Link className={linkClasses} href="https://www.facebook.com/loabb">
         <FacebookIcon />
       </Link>
       <Link
-        className="bg-white/60 dark:bg-black/60 p-4 rounded-full"
+        className={linkClasses}
         href="https://www.instagram.com/loabragadottir"
       >
         <InstagramIcon />
       </Link>
       <Link
-        className="bg-white/60 dark:bg-black/60 p-4 rounded-full"
+        className={linkClasses}
         href="https://www.linkedin.com/in/%C3%B3l%C3%B6f-bj%C3%B6rk-bragad%C3%B3ttir-8baa0426/"
       >
         <LinkedInIcon />
