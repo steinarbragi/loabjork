8fdfb4d73ddf7b68fb1fad8d203f1d06c21a4075
Steinar Bragi Sigurðarson <s@steinar.io>
2023-12-28 15:40:07 +0000

    social icons

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,7 +1,26 @@
+import FacebookIcon from '@/icons/facebook';
+import InstagramIcon from '@/icons/instagram';
+import LinkedInIcon from '@/icons/linkedin';
+import Link from 'next/link';
+
 export default function Hero() {
   return (
     <div>
       <h1 className="text-center text-3xl font-bold">Lóa Björk</h1>
+      <div
+        id="social"
+        className="mx-auto my-10 text-center flex flex-row space-x-6 justify-center"
+      >
+        <Link href="https://www.facebook.com/loabb">
+          <FacebookIcon />
+        </Link>
+        <Link href="https://www.instagram.com/loabragadottir">
+          <InstagramIcon />
+        </Link>
+        <Link href="https://www.linkedin.com/in/%C3%B3l%C3%B6f-bj%C3%B6rk-bragad%C3%B3ttir-ab81a3119/">
+          <LinkedInIcon />
+        </Link>
+      </div>
     </div>
   );
 }
