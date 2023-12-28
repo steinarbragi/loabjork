0c1c5b1c8a7c727dfaf0e5a0a80adfb3fc4f3b18
Steinar Bragi Sigurðarson <s@steinar.io>
2023-12-28 18:48:23 +0000

    tweak social buttons

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <div className="w-100 h-48">
+    <div className="w-100">
       <h1 className="text-center text-6xl font-bold">Lóa Björk</h1>
     </div>
   );
--- src/components/social.tsx
+++ src/components/social.tsx
@@ -12,19 +12,19 @@ export default function Social({ color }: { color?: string }) {
       } mx-auto text-center flex flex-row space-x-6 justify-center`}
     >
       <Link
-        className="bg-white/60 dark:bg-black/40 p-4 rounded-full"
+        className="bg-white/60 dark:bg-black/60 p-4 rounded-full"
         href="https://www.facebook.com/loabb"
       >
         <FacebookIcon />
       </Link>
       <Link
-        className="bg-white/60 dark:bg-black/40 p-4 rounded-full"
+        className="bg-white/60 dark:bg-black/60 p-4 rounded-full"
         href="https://www.instagram.com/loabragadottir"
       >
         <InstagramIcon />
       </Link>
       <Link
-        className="bg-white/60 dark:bg-black/40 p-4 rounded-full"
+        className="bg-white/60 dark:bg-black/60 p-4 rounded-full"
         href="https://www.linkedin.com/in/%C3%B3l%C3%B6f-bj%C3%B6rk-bragad%C3%B3ttir-8baa0426/"
       >
         <LinkedInIcon />
