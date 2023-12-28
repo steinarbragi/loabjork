0ced530d64f68acf8a61f6b2e0d26875815b4ce6
Steinar Bragi Sigurðarson <s@steinar.io>
2023-12-28 18:32:19 +0000

    mute video

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -7,7 +7,7 @@ export default function Video() {
           width="100%"
           height="100%"
           className="-z-50"
-          src="https://www.youtube.com/embed/lceWbYCaMhg?si=hAUnblZ_IYaIFuXR&amp;controls=0&autoplay=1"
+          src="https://www.youtube.com/embed/lceWbYCaMhg?si=hAUnblZ_IYaIFuXR&amp;controls=0&autoplay=1&mute=1"
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
