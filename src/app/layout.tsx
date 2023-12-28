8c731ecbdfb89c00c1c791fb1211783fb2433c7a
Steinar Bragi Sigurðarson <s@steinar.io>
2023-12-28 22:54:12 +0000

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ReactNode } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -33,7 +33,7 @@ export default function Navigation() {
   return (
     <Disclosure
       as="nav"
-      className="bg-white/50 dark:bg-black/30 fixed right-0 left-0 backdrop-blur shadow"
+      className="bg-white/50 dark:bg-black/60 fixed right-0 left-0 backdrop-blur shadow"
     >
       {({ open }) => (
         <>
