aa6f09618357c68b83861d0f7f5b5d2b7ae5e2ee
Steinar Bragi Sigurðarson <s@steinar.io>
2023-12-29 14:55:39 +0000

    add slug field to publications

--- sanity/schemas/publication.js
+++ sanity/schemas/publication.js
@@ -8,7 +8,16 @@ export default {
       type: 'string',
       title: 'Titill',
     },
-
+    {
+      title: 'Slug',
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 200,
+        slugify: input => input.toLowerCase().replace(/\s/g, '-').slice(0, 200),
+      },
+    },
     {
       name: 'date',
       type: 'date',
