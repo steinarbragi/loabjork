040402c9393e81de7a05756326369b400880f32d
Steinar Bragi Sigurðarson <s@steinar.io>
2023-12-29 00:38:47 +0000

    unused imports

--- src/components/contactForm.tsx
+++ src/components/contactForm.tsx
@@ -1,9 +1,3 @@
-import {
-  BuildingOffice2Icon,
-  EnvelopeIcon,
-  PhoneIcon,
-} from '@heroicons/react/24/outline';
-
 const textFieldClasses =
   'dark:bg-slate-900 block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-800 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6';
 
